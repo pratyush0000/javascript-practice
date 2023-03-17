f3beff7f23d2e53dfcb0dd3239b6b40f8c6c062f
Pratyush Kamal <pratyushkamal88@gmail.com>
2023-03-17 06:27:10 +0530

    save button added to passenger counter

--- Passenger counter app/index.js
+++ Passenger counter app/index.js
@@ -1,10 +1,19 @@
 //document.getElementById("count").innerText=5
 
-let count = 0
 
+let getcount = document.getElementById("count")
 // console.log(count)
-
+let jscount = 0
 function increment(){
-    count=count+1
-    console.log(count)
+    jscount+=1
+    getcount.innerText=jscount
 }
+
+let getentries = document.getElementById("entries")
+
+function save(){
+    getentries.innerText+=jscount + "->"
+}
+
+
+
