cf8c982681e709e4a94f72eda3ee2a9cb7ecbb24
Pratyush Kamal <pratyushkamal88@gmail.com>
2023-08-17 16:36:17 +0530

    return

--- Passenger counter app/index.js
+++ Passenger counter app/index.js
@@ -7,11 +7,13 @@ let jscount = 0
 function increment(){
     jscount+=1
     getcount.textContent=jscount
+    console.log(jscount)
 }
 
 let getentries = document.getElementById("entries")
 
 function save(){
+    console.log(jscount)
     getentries.textContent+=jscount + "->"
     jscount=0
     getcount.textContent=0
