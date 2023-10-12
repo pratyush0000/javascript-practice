c0f73bbfbdb82d37a4fcdf7224f1994663b8655e
Pratyush Kamal <pratyushkamal88@gmail.com>
2023-10-12 09:59:08 +0530

    update

--- Linklet/index.js
+++ Linklet/index.js
@@ -4,10 +4,24 @@ const inputButton = document.getElementById("inputButton")
 const inputText = document.getElementById("inputText")
 let ulEl = document.getElementById("ulEl")
 
+
+
+localStorage.clear()
+let ls = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(ls)
+
+
 inputButton.addEventListener("click", function(){
     myLeads.push(inputText.value)
+
     inputText.value=""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    
     printList()
+
+
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function printList(){
